338a63799dac9297a5fb617c86bcaf6f966b67c8
nogoup <nogoup.antoine@gmail.com>
2021-02-26 12:42:36 +0100

    second push

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,15 +8,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test', 'this is simply a test','https://live.staticflickr.com/65535/48588252551_16d7043332_h.jpg'),
-    new Recipe('A test', 'this is simply a test','https://live.staticflickr.com/65535/48588252551_16d7043332_h.jpg')
+    new Recipe('B test', 'this is the second test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?webp=true&quality=90&resize=440%2C400')
   ];
+
+  public selectedRecipe!: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
+
+  detail(recipe: Recipe){
+    this.selectedRecipe = recipe;
   }
 
 }
